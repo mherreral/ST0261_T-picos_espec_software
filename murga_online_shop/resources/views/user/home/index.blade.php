501973101889e54317bd986ab2ad46af0b77112a
Manuela <mherreral@eafit.edu.co>
2022-03-21 15:23:26 -0500

    Added css and views template

---
+++ murga_online_shop/resources/views/user/home/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Home Page - Online Store')
+@section('content')
+    <div class="text-center">
+        {{ __('messages.home.welcome') }}
+    </div>
+@endsection
