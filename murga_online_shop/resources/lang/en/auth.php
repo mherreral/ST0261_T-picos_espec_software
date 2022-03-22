a7ca418e5a21af8c1d5f227221a04caa8be6ba47
Manuela <mherreral@eafit.edu.co>
2022-03-21 19:10:47 -0500

    added auth messages

---
+++ murga_online_shop/resources/lang/en/auth.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'register' => 'Register',
+    'email' => 'Email address',
+    'password' => 'Password',
+    'name' => 'Name',
+    'confirm' => 'Confirm assword',
+    'login' => 'Login',
+    'remember' => 'Forgot your password?',
+    'verification' => 'Verify Your Email Address',
+    'verificationLink' => 'A fresh verification link has been sent to your email address',
+    'checkVerificationLink' => 'Before proceeding, please check your email for a verification link.',
+    'emailNotReceived' => 'If you did not receive the email',
+    'idNumber' => 'ID card number',
+    'address' => 'Deliever address',
+    'birthDate' => 'Birthdate',
+    'phoneNumber' => 'Phone number',
+
+    //Passwords
+    'confirmPassword' => 'Confirm Password',
+    'askForConfirmation' => 'Please confirm your password before continuing.',
+    'resetPassword' => 'Reset Password',
+    'sendResetLink' => 'Send Password Reset Link'
+];
