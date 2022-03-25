7b8fe05fe1ce4a76ced87bbec72d46f8feea0d0f
Manuela <mherreral@eafit.edu.co>
2022-03-25 14:39:56 -0500

    addes user liquor controller

---
+++ murga_online_shop/app/Http/Controllers/User/LiquorController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\Models\Liquor;
+
+class LiquorController extends Controller
+{
+    public function index()
+    {
+        $viewData = [];
+        $viewData['title'] = __('messages.shop.title');
+        $viewData['liquors'] = Liquor::all();
+        return view('user.liquor.index')->with("viewData", $viewData);
+    }
+
+    public function show($id)
+    {
+        $viewData = [];
+        $liquor = Liquor::findOrFail($id);
+        $loggedUser = Auth::user();
+        $userWishlists = [];
+        foreach ($loggedUser->wishlists as $wishlist) {
+            array_push($userWishlists, $wishlist);
+        }
+        $viewData["title"] = $liquor->getName();
+        $viewData["liquor"] = $liquor;
+        $viewData["wishlists"] = $userWishlists;
+        return view('user.liquor.show')->with("viewData", $viewData);
+    }
+}
---
+++ murga_online_shop/resources/views/user/liquor/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('title', $viewData['title'])
+@section('content')
+    <div class="row">
+        @foreach ($viewData['liquors'] as $liquor)
+            <div class="col-md-4 col-lg-3 mb-2">
+                <div class="card">
+                    <img src="{{ asset('/storage/' . $liquor->getImage()) }}" class="card-img-top img-card">
+                    <div class="card-body text-center">
+                        <a href="{{ route('user.liquor.show', ['id' => $liquor->getId()]) }}"
+                            class="btn bg-primary text-white">{{ $liquor->getName() }}</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
