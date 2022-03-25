95cb17fc50eaa1eea20d7133583091af0b8f382e
Samuel Palacios Bernate <sdpalaciob@eafit.edu.co>
2022-03-24 21:17:15 -0500

    Add routes and views for customers admin panel

---
+++ murga_online_shop/resources/views/admin/customer/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('title', $viewData['subtitle'])
+@section('subtitle', $viewData['subtitle'])
+@section('content')
+    <div class="row">
+        @foreach ($viewData['customers'] as $customer)
+            <div class="col-md-4 col-lg-3 mb-2">
+                <div class="card">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+                        class="card-img-top img-card">
+                    <div class="card-body text-center">
+                        <a href="{{ route('admin.customer.show', ['id' => $customer->getId()]) }}"
+                            class="btn bg-primary text-white">{{ $customer->getName() }}</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
--- murga_online_shop/routes/web.php
+++ murga_online_shop/routes/web.php
@@ -20,7 +20,36 @@ Route::get('/', 'App\Http\Controllers\User\HomeController@index')->name("user.ho
 
 // Auth needed
 Route::middleware('admin')->group(function () {
-    Route::get('/admin', 'App\Http\Controllers\Admin\HomeController@index')->name('admin.home.index');
+
+    Route::get(
+        '/admin',
+        'App\Http\Controllers\Admin\HomeController@index'
+    )->name('admin.home.index');
+
+    Route::get(
+        '/admin/customer',
+        'App\Http\Controllers\Admin\CustomerController@index'
+    )->name("admin.customer.index");
+
+    Route::get(
+        '/admin/customer/setadmin',
+        'App\Http\Controllers\Admin\CustomerController@setAdmin'
+    )->name("admin.customer.setAdmin");
+
+    Route::get(
+        '/admin/customer/delete',
+        'App\Http\Controllers\Admin\CustomerController@delete'
+    )->name("admin.customer.delete");
+
+    Route::post(
+        '/admin/customer/save',
+        'App\Http\Controllers\Admin\CustomerController@save'
+    )->name("admin.customer.save");
+
+    Route::get(
+        '/admin/customer/{id}',
+        'App\Http\Controllers\Admin\CustomerController@show'
+    )->name("admin.customer.show");
 });
 
 Route::get('locale/{locale}', 'App\Http\Controllers\LocalizationController@locale')->name('locale');
