9f65687fa72aa3654bf4579fecb7189cc8605391
Manuela <mherreral@eafit.edu.co>
2022-03-28 00:04:22 -0500

    Added new routes to shopping cart and reordered them

--- murga_online_shop/routes/web.php
+++ murga_online_shop/routes/web.php
@@ -18,8 +18,8 @@ use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
-Route::get('/', 'App\Http\Controllers\User\HomeController@index')->name("user.home.index");
-
+//Route::get('/', 'App\Http\Controllers\User\HomeController@index')->name("user.home.index");
+Route::get('/', 'App\Http\Controllers\User\LiquorController@index')->name("user.home.index");
 // Auth needed
 
 //Admin
@@ -30,16 +30,23 @@ Route::middleware('admin')->group(function () {
 //User
 
 Route::middleware('auth')->group(function () {
+    //Wishlists
     Route::get('/wishlists', 'App\Http\Controllers\User\WishlistController@index')->name("user.wishlist.index");
     Route::get('/wishlists/create', 'App\Http\Controllers\User\WishlistController@create')->name("user.wishlist.create");
     Route::post('/wishlists/save', 'App\Http\Controllers\User\WishlistController@save')->name("user.wishlist.save");
     Route::get('/wishlists/{id}', 'App\Http\Controllers\User\WishlistController@show')->name("user.wishlist.show");
     Route::delete('/wishlists/delete/{id}', 'App\Http\Controllers\User\WishlistController@delete')->name("user.wishlist.delete");
+    Route::post('/wishlist/add/{id}', 'App\Http\Controllers\User\WishlistController@addItem')->name('user.wishlist.add');
+
+    //liquors
     Route::get('/liquors', 'App\Http\Controllers\User\LiquorController@index')->name("user.liquor.index");
     Route::get('/liquors/{id}', 'App\Http\Controllers\User\LiquorController@show')->name("user.liquor.show");
-    Route::post('/wishlist/add/{id}', 'App\Http\Controllers\User\WishlistController@addItem')->name('user.wishlist.add');
+
+    //Cart
     Route::post('/cart/add/{id}', 'App\Http\Controllers\User\ShoppingCartController@add')->name("user.shoppingCart.add");
     Route::get('/cart', 'App\Http\Controllers\User\ShoppingCartController@index')->name("user.shoppingCart.index");
+    Route::get('/cart/purchase', 'App\Http\Controllers\User\ShoppingCartController@purchase')->name('user.shoppingCart.purchase');
+    Route::get('/cart/delete', 'App\Http\Controllers\User\ShoppingCartController@delete')->name('user.shoppingCart.delete');
 });
 
 Route::get('locale/{locale}', 'App\Http\Controllers\LocalizationController@locale')->name('locale');
