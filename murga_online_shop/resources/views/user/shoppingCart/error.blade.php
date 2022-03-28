ee573f7d1d551e0e0a25fa263f6c52184cd335ce
Manuela <mherreral@eafit.edu.co>
2022-03-28 00:07:00 -0500

    Added new views to cart

---
+++ murga_online_shop/resources/views/user/shoppingCart/error.blade.php
@@ -0,0 +1,7 @@
+<!-- Authors: Manuela Herrera López -->
+@extends('layouts.app')
+@section('content')
+    <div class="text-center">
+        <p>{{ $error }}</p>
+    </div>
+@endsection
---
+++ murga_online_shop/resources/views/user/shoppingCart/purchase.blade.php
@@ -0,0 +1,7 @@
+<!-- Authors: Manuela Herrera López -->
+@extends('layouts.app')
+@section('content')
+    <div class="text-center">
+        {{ __('messages.shoppinCart.purchase.done') }}
+    </div>
+@endsection
