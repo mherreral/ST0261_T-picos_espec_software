f7660b078c06ed75a599ee45bad76faf1f484101
Samuel Palacios Bernate <sdpalaciob@eafit.edu.co>
2022-03-29 23:35:54 -0500

    Fix bad path to default image

--- murga_online_shop/resources/views/user/liquor/show.blade.php
+++ murga_online_shop/resources/views/user/liquor/show.blade.php
@@ -5,7 +5,7 @@
     <div class="card mb-3">
         <div class="row g-0">
             <div class="col-md-4">
-                <img src="{{ asset('/storage/' . $viewData['liquor']->getImage()) }}" class="img-fluid rounded-start">
+                <img src="{{ asset($viewData['liquor']->getImage()) }}" class="img-fluid rounded-start">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
