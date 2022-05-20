4484e70979d428e53ef85a2230739421e40107e0
mherreral <mherreral@eafit.edu.co>
2022-05-19 19:40:03 -0500

    added spanish version of the app

---
+++ murga_online_shop/resources/lang/es/auth.php
@@ -0,0 +1,27 @@
+<?php
+
+//Authors: Manuela Herrera López
+
+return [
+    'register' => 'Regístrate',
+    'email' => 'Correo electrónico',
+    'password' => 'Contraseña',
+    'name' => 'Nombre',
+    'confirm' => 'Confirma tu contraseña',
+    'login' => 'Ingreso',
+    'remember' => '¿Olvidaste tu contraseña?',
+    'verification' => 'Verifica tu correo electrónico',
+    'verificationLink' => 'Se ha enviado un link de verificación a tu correo',
+    'checkVerificationLink' => 'Antes de seguir, por favor verifique su correo',
+    'emailNotReceived' => 'Si no recibiste el correo',
+    'idNumber' => 'Número de cédula',
+    'address' => 'Dirección de entrega',
+    'birthDate' => 'Fecha de nacimiento',
+    'phoneNumber' => 'Número de teléfono',
+
+    //Passwords
+    'confirmPassword' => 'Confirma tu contraseña',
+    'askForConfirmation' => 'Confirma tu contraseña antes de seguir.',
+    'resetPassword' => 'Recupera tu contraseña',
+    'sendResetLink' => 'Enviar link de recuperación de contraseña'
+];
---
+++ murga_online_shop/resources/lang/es/messages.php
@@ -0,0 +1,108 @@
+<?php
+
+// Authors: Manuela Herrera López, Samuel Palacios, Ana Arango
+
+return [
+    //Homepage
+    'home.title' => 'LA MURGA - Licores',
+    'home.store' => 'Tienda',
+    'home.wishlist' => 'Wishlists',
+    'home.copyright' => 'Derechos de autor - ',
+    'home.author' => 'Daniel Correa',
+    'home.welcome' => '¡Bienvenidos a nuestra tienda!',
+    'home.admin' => 'Panel de administración',
+    'home.shoppingCart' => 'Carrito de compras',
+    'home.goBackHome' => 'Volver a la página de inicio',
+    'languages' => 'Languages',
+
+    //Auth
+    'auth.login' => 'Ingreso',
+    'auth.register' => 'Regístrate',
+    'auth.logout' => 'Salir',
+
+    //User
+    'user.customer.availableMoney' => 'Dinero disponible',
+
+    //Admin
+    'admin.title' => 'Panel de administración',
+    'admin.createLiquors' => 'Crear licor',
+    'admin.manageLiquors' => 'Editar licores',
+    'admin.customer.setAdmin' => 'Hacer admin',
+    'admin.customer.deleteCustomer' => 'Eliminar usuario',
+    'admin.customer.manage' => 'Editar usuario',
+    'admin.customer.listOf' => 'Lista de usuarios',
+    'admin.customer.showTitle' => ' Murga',
+    'admin.customer.showSubTitle' => ' Información',
+    'admin.customer.setAdminTitle' => 'Volver administrador',
+    'admin.customer.deleteTitle' => 'Eliminación de usuario',
+    'admin.customer.saveTitle' => 'Usuario actualizado',
+    'admin.customer.saveSubTitle' => 'Operación exitosa',
+    'admin.customer.standardFlag' => 'Estándar',
+    'admin.customer.adminFlag' => 'Admin',
+    'admin.goBackHome' => 'Volver al panel de administración',
+    'admin.name' => ':name',
+    'admin.message' => 'Bienvenido al panel de admin, use la barra de navegación para hacer cambios',
+    'admin.createLiquorsType' => 'Ingresa el tipo',
+    'admin.createLiquorsBrand' => 'Ingresa la marca',
+    'admin.createLiquorsPrice' => 'Ingresa el precio',
+    'admin.createLiquorsStock' => 'Ingresa el stock',
+    'admin.createLiquorsPresentation' => 'Ingresa la presentación',
+    'admin.createLiquorsMilliliters' => 'Ingresa los mililitros',
+    'admin.createLiquorsImage' => 'Imagen',
+    'admin.saveLiquorsSuccess' => 'Licor creado exitosamente',
+    'admin.deleteLiquorsSuccess' => 'Licor eliminado exitosamente',
+    'admin.delete.liquor' => 'Eliminar',
+    'admin.update.liquor' => 'Actualizar',
+    'admin.editeLiquors' => "Editar Licores",
+    'admin.updateLiquorsSuccess' => 'Licor actualizado exitosamente',
+
+    //Shop
+    'shop.title' => 'Tienda de licores',
+
+    //Liquor
+    'liquor.quantity' => 'Cantidad',
+    'liquor.wishlist' => 'Wishlist',
+    'liquor.commentsMean' => 'Puntuación promedio',
+    'wishlist.add' => 'Agregar a wishlist',
+    'liquor.search' => 'Buscar licor',
+    'liquor.priceASC' => 'Precio ascendente',
+    'liquor.priceDESC' => 'Precio descendente',
+    'liquor.price' => 'Precio ($): ',
+    'liquor.milliliters' => 'Mililitros (ml): ',
+    'liquor.presentation' => 'Presentación: ',
+    'liquor.stock' => 'Stock: ',
+
+    //Comments
+    'comment.createComments' => 'Crear comentario',
+    'comment.saveCommentsSuccess' => 'Comentario agregado satisfactoriamente',
+    'comment.createCommentDescription' => 'Descripción del comentario',
+    'comment.score' => 'Puntaje',
+    'comment.sendComment' => 'Enviar comentario',
+
+    //Wishlist
+    'wishlist.title' => 'Wishlists - LA MURGA',
+    'wishlist.create' => 'Crear wishlist',
+    'wishlist.name' => 'Nombre de la wishlist',
+    'wishlist.delete' => 'Eliminar wishlist',
+    'wishlist.customers' => 'Agregar email de los colaboradores (Lista separada por punto y coma)',
+    'wishlist.liquor' => 'Licor:',
+    'wishlist.liquor.quantity' => 'Cantidad:',
+    'wishlist.toCart' => 'Agregar wishlist al carrito',
+    'wishlist.email.error' => 'No se pudo encontrar un usuario regiostrado con uno de los emails dados',
+
+    //Shoppingcart
+    'shoppingCart.title' => 'Carrito de licores',
+    'shoppingCart.purchase.title' => 'La murga - compra',
+    'shoppingCart.aux' => '',
+    'shoppingCart.success' => 'Compra hecha satisfactoriamente',
+    'shoppingCart.error' => 'No tienes dinero suficiente para completar la compra',
+    'shoppingCart.wishlists' => 'Wishlists en el carrito',
+    'shoppingCart.wishlists.item.name' => 'Nombre',
+    'shoppingCart.wishlists.item.total' => 'Total',
+    'shoppingCart.wishlists.item.quantity' => 'Cantidad',
+    'shoppingCart.wishlists.total' => 'Total a pagar',
+    'shoppingCart.purchase' => 'Comprar',
+    'shoppingCart.delete' => 'Eliminar todos los productos del carrito',
+    'shoppinCart.purchase.done' => 'La compra fue exitosa',
+    'shoppingCart.error.outOfStock' => 'Lo sentimos, no tenemos suficiente stock para que completes la compra',
+];
